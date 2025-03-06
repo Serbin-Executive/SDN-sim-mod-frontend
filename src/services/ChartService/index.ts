1446c09394bd866bdc755bdbf3f59f380dbbc350
Ivan Rybin <rybin1van@outlook.com>
2025-03-06 16:25:58 +0300

    [#A2] Added range to visible charts data

--- src/services/ChartService/index.ts
+++ src/services/ChartService/index.ts
@@ -1,18 +1,29 @@
 import { IChartData, TChartLabels } from "@/components/LineCharts/meta";
 import { TModelCurrentStates, TObjectsStatesInfo } from "@/hooks/useServerMessageHandler/meta";
+import { CHART_VISIBLE_DOTS_COUNT } from "./meta";
 
 export class ChartService {
-    public static getChartLabels(data: TModelCurrentStates): number[] {
+    public static getChartLabels(data: TModelCurrentStates): TChartLabels {
         return data.map((dataItem) => Number(dataItem.time));
     }
 
+    public static getVisibleData(data: any[]): any[] {
+        const firstIndex: number = Math.max(0, data.length - CHART_VISIBLE_DOTS_COUNT);
+
+        return data.slice(firstIndex, data.length);
+    }
+
     public static getChartData(labelName: string, modelCurrentStates: TModelCurrentStates, data: number[], color: string): IChartData {
+        const visibleChartLabels = this.getVisibleData(this.getChartLabels(modelCurrentStates));
+        const visibleData = this.getVisibleData(data);
+
+
         return {
-            labels: this.getChartLabels(modelCurrentStates),
+            labels: visibleChartLabels,
             datasets: [
                 {
                     label: labelName,
-                    data: data,
+                    data: visibleData,
                     borderColor: color,
                     pointBackgroundColor: color,
                     pointBorderColor: color,
