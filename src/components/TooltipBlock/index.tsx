81357de51eaac70cf2e4ce5da9efecd89852eeff
Andrew Serbin <serbinadrian@outlook.com>
2025-03-23 00:42:39 +0300

    Restyling fixes on merge

--- src/components/TooltipBlock/index.tsx
+++ src/components/TooltipBlock/index.tsx
@@ -1,24 +1,20 @@
-import { type ReactNode, type ReactElement } from "react";
+import { type ReactElement } from "react";
 import "./style.css";
 
 export type TTooltipDirection = string;
 
 export const TOOLTIP_DIRECTION: string = "top";
-export const TOOLTIP_LABEL_TEXT: string = "ⓘ";
 
 export interface ITooltipBlockProps {
-    children?: ReactNode;
     flow?: TTooltipDirection;
     info: string;
 }
 
 const TooltipBlock = ({
-    children,
     info,
     flow = TOOLTIP_DIRECTION,
 }: ITooltipBlockProps): ReactElement => (
     <div className="tooltip-block">
-        <div className="title">{children}</div>
         <div data-tooltip={info} data-flow={flow} className="label">
             <svg
                 version="1.1"
